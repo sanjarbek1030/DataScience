9d7fd9b04a563dd1c66a3cab08f335ef9a4798c3
Jumaboev Sanjarbek <55908630+sanjarbek1030@users.noreply.github.com>
2026-09-23 18:43:42 +0900

    Add files via upload

---
+++ intendation.py
@@ -0,0 +1,24 @@
+"""
+*********
+INTENDATION (JOY TASHLASH)
+*********
+"""
+# whitespace is ignored inside (), []
+long_winded_computation = (1 + 2 + 3 + 4 + 5 + 6 + 7 + 
+                           8 + 9 + 10 + 11 + 12 + 13 + 14 + 
+                           15 + 16 + 17 + 18 + 19 + 20)
+
+list_of_lists = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+easier_to_read_list_of_lists = [[1, 2, 3],
+                                [4, 5, 6],
+                                [7, 8, 9]]
+
+# we can use backslash to continue onto the next line
+two_plus_three = 2 + \
+                 3
+
+
+print(long_winded_computation)
+print(easier_to_read_list_of_lists)
+print(two_plus_three)
