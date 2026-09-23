90b6ab06cd10c2a1f6f2e8aff0b912bf071e7b4f
Jumaboev Sanjarbek <55908630+sanjarbek1030@users.noreply.github.com>
2026-09-23 19:05:18 +0900

    Add files via upload

---
+++ tuples.py
@@ -0,0 +1,28 @@
+"""
+****************
+TUPLES
+****************
+"""
+
+my_list = [1, 2]
+my_tuple = (1, 2)
+other_tuple = 1, 2
+
+my_list[1] = 3
+
+try:
+    my_tuple[1] = 3
+except TypeError:
+    print("Cannot modify a tuple")
+
+def sum_and_product(x, y):
+    return (x + y), (x * y)
+
+sp = sum_and_product(2, 3) # sp is (5, 6)
+s, p = sum_and_product(5, 10) # s is 15, p is 50
+
+print("sp value: ", sp)
+print(f"s value: {s}, p value: {p}")
+
+x, y = 1, 2 # now x is 1, y is 2
+x, y = y, x # Pythonic way to swap variables; now x is 2, y is 1
